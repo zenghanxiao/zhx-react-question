5811dd8df46289264f0ea3a98b9686fe74bc5fd4
zenghanxiao <zengxiao505685546@qq.com>
2025-09-14 22:52:51 +0800

    配置prettier

--- eslint.config.js
+++ eslint.config.js
@@ -1,16 +1,17 @@
-import js from "@eslint/js"
-import globals from "globals"
-import reactHooks from "eslint-plugin-react-hooks"
-import reactRefresh from "eslint-plugin-react-refresh"
-import tseslint from "typescript-eslint"
-import { globalIgnores } from "eslint/config"
+import js from '@eslint/js'
+import globals from 'globals'
+import reactHooks from 'eslint-plugin-react-hooks'
+import reactRefresh from 'eslint-plugin-react-refresh'
+import tseslint from 'typescript-eslint'
+import { globalIgnores } from 'eslint/config'
+import eslintConfigPrettier from 'eslint-config-prettier'
 // import reactX from "eslint-plugin-react-x"
 // import reactDom from "eslint-plugin-react-dom"
 
 export default tseslint.config([
-  globalIgnores(["dist"]),
+  globalIgnores(['dist']),
   {
-    files: ["**/*.{ts,tsx}"],
+    files: ['**/*.{ts,tsx}'],
     extends: [
       js.configs.recommended,
       tseslint.configs.recommended,
@@ -20,20 +21,29 @@ export default tseslint.config([
       // ...tseslint.configs.strictTypeChecked,
       // // Optionally, add this for stylistic rules
       // ...tseslint.configs.stylisticTypeChecked,
-      reactHooks.configs["recommended-latest"],
+      reactHooks.configs['recommended-latest'],
       reactRefresh.configs.vite,
       // // Enable lint rules for React
       // reactX.configs["recommended-typescript"],
       // // Enable lint rules for React DOM
       // reactDom.configs.recommended,
     ],
+    plugins: ['react', '@typescript-eslint'],
     languageOptions: {
       ecmaVersion: 2020,
       globals: globals.browser,
       parserOptions: {
-        project: ["./tsconfig.node.json", "./tsconfig.app.json"],
+        project: ['./tsconfig.node.json', './tsconfig.app.json'],
         tsconfigRootDir: import.meta.dirname,
       },
+    },
+  },
+  {
+    files: ['**/*.{ts,tsx}'],
+    extends: [eslintConfigPrettier],
+    plugins: ['prettier'],
+    rules: {
+      'prettier/prettier': 'error',
     },
   },
 ])
---
+++ src/test.ts
@@ -0,0 +1,3 @@
+const a = '!23'
+
+export default a
