662fc4afb4867117e07208c7fa820493e23b98f9
zenghanxiao <zengxiao505685546@qq.com>
2025-09-14 22:21:33 +0800

    Add recommended eslint config

--- eslint.config.js
+++ eslint.config.js
@@ -1,23 +1,39 @@
-import js from '@eslint/js'
-import globals from 'globals'
-import reactHooks from 'eslint-plugin-react-hooks'
-import reactRefresh from 'eslint-plugin-react-refresh'
-import tseslint from 'typescript-eslint'
-import { globalIgnores } from 'eslint/config'
+import js from "@eslint/js"
+import globals from "globals"
+import reactHooks from "eslint-plugin-react-hooks"
+import reactRefresh from "eslint-plugin-react-refresh"
+import tseslint from "typescript-eslint"
+import { globalIgnores } from "eslint/config"
+// import reactX from "eslint-plugin-react-x"
+// import reactDom from "eslint-plugin-react-dom"
 
 export default tseslint.config([
-  globalIgnores(['dist']),
+  globalIgnores(["dist"]),
   {
-    files: ['**/*.{ts,tsx}'],
+    files: ["**/*.{ts,tsx}"],
     extends: [
       js.configs.recommended,
       tseslint.configs.recommended,
-      reactHooks.configs['recommended-latest'],
+      // // Remove tseslint.configs.recommended and replace with this
+      // ...tseslint.configs.recommendedTypeChecked,
+      // // Alternatively, use this for stricter rules
+      // ...tseslint.configs.strictTypeChecked,
+      // // Optionally, add this for stylistic rules
+      // ...tseslint.configs.stylisticTypeChecked,
+      reactHooks.configs["recommended-latest"],
       reactRefresh.configs.vite,
+      // // Enable lint rules for React
+      // reactX.configs["recommended-typescript"],
+      // // Enable lint rules for React DOM
+      // reactDom.configs.recommended,
     ],
     languageOptions: {
       ecmaVersion: 2020,
       globals: globals.browser,
+      parserOptions: {
+        project: ["./tsconfig.node.json", "./tsconfig.app.json"],
+        tsconfigRootDir: import.meta.dirname,
+      },
     },
   },
 ])
